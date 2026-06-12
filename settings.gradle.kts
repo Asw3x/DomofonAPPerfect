09dcd653b8dac02c7c2cbe25c62faae0df566a90
unknown <asw3x.is@@gmail.com>
2026-06-12 20:32:45 +0300

    Legal rebranding: renamed to DomofonAPPerfect

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -31,5 +31,5 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-rootProject.name = "DomonapPerfect"
+rootProject.name = "DomofonAPPerfect"
 include(":app")
